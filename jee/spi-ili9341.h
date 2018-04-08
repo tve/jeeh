f333a8eaa748e9aab3d8da21549f373f84aae024
Thorsten von Eicken <tve@voneicken.com>
2018-04-08 08:15:01 -0700

    waterfall2 using two rfm96 radios

--- jee/spi-ili9341.h
+++ jee/spi-ili9341.h
@@ -3,21 +3,22 @@
 
 template< typename MO, typename MI, typename CK, typename SS, typename DC>
 struct ILI9341 {
-    constexpr static int width = 320;
-    constexpr static int height = 240;
+    constexpr static int width = 240;
+    constexpr static int height = 320;
 
     static void init () {
         dc = 1;
         dc.mode(Pinmode::out);
+        spi.init();
         spi.enable();
 
         for (uint8_t const* p = config; p < config + sizeof config; ++p) {
             cmd(*p);
             int n = *++p;
             while (--n >= 0)
-                cmd(*++p);
+                spi.transfer(*++p);
         }
-    
+
         wait_ms(120);
         cmd(0x29);      // DISPON
         spi.disable();
@@ -34,7 +35,7 @@ struct ILI9341 {
         spi.transfer(v);
     }
 
-    static void pixel (int y, int x, uint16_t rgb) {
+    static void pixel (int x, int y, uint16_t rgb) {
         spi.enable();
 
         cmd(0x2A);
@@ -51,7 +52,7 @@ struct ILI9341 {
         spi.disable();
     }
 
-    static void pixels (int y, int x, uint16_t const* rgb, int len) {
+    static void pixels (int x, int y, uint16_t const* rgb, int len) {
         pixel(y, x, *rgb);
 
         spi.enable();
@@ -69,33 +70,33 @@ struct ILI9341 {
         spi.disable();
     }
 
-    static SpiDev<MO,MI,CK,SS,1> spi;
+    static SpiDev<MO,MI,CK,SS,0> spi;
     static DC dc;
     static uint8_t const config [];
 };
 
 template< typename MO, typename MI, typename CK, typename SS, typename DC>
-SpiDev<MO,MI,CK,SS,1> ILI9341<MO,MI,CK,SS,DC>::spi;
+SpiDev<MO,MI,CK,SS,0> ILI9341<MO,MI,CK,SS,DC>::spi;
 
 template< typename MO, typename MI, typename CK, typename SS, typename DC>
 DC ILI9341<MO,MI,CK,SS,DC>::dc;
 
 template< typename MO, typename MI, typename CK, typename SS, typename DC>
 uint8_t const ILI9341<MO,MI,CK,SS,DC>::config [] = {
-    0xEF, 3, 0x03, 0x80, 0x02,
-    0xCF, 3, 0x00, 0xC1, 0x30,
-    0xED, 4, 0x64, 0x03, 0x12, 0x81,
-    0xE8, 3, 0x85, 0x00, 0x78,
-    0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,
-    0xF7, 1, 0x20,
-    0xEA, 2, 0x00, 0x00,
+    0xEF, 3, 0x03, 0x80, 0x02,               // ??
+    0xCF, 3, 0x00, 0xC1, 0x30,               // power control B
+    0xED, 4, 0x64, 0x03, 0x12, 0x81,         // power on sequence control
+    0xE8, 3, 0x85, 0x00, 0x78,               // driver timing control A
+    0xCB, 5, 0x39, 0x2C, 0x00, 0x34, 0x02,   // power control A
+    0xF7, 1, 0x20,                           // pump ration control
+    0xEA, 2, 0x00, 0x00,                     // driver timing control B
     0xC0, 1, 0x23,                           // PWCTR1
     0xC1, 1, 0x10,                           // PWCTR2
     0xC5, 2, 0x3e, 0x28,                     // VMCTR1
     0xC7, 1, 0x86,                           // VMCTR2
-    0x36, 1, 0x48,                           // MADCTL
-    0x37, 2, 0, 0,                           // VSCRSADD
-    0x3A, 1, 0x55,                           // PIXFMT
+    0x36, 1, 0x88,                           // memory access ctrl: column order, BGR filter
+    0x37, 2, 0, 0,                           // vertical scrolling start
+    0x3A, 1, 0x55,                           // pixel format: 16 bits/pixel
     0xB1, 2, 0x00, 0x18,                     // FRMCTR1
     0xB6, 3, 0x08, 0x82, 0x27,               // DFUNCTR
     0xF2, 1, 0x00,
