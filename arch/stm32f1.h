1870608b9650453e5a1f7e054abf7866c3a44cc6
Jean-Claude Wippler <jc@wippler.nl>
2018-04-30 11:42:38 +0200

    clean up Periph constants

--- arch/stm32f1.h
+++ arch/stm32f1.h
@@ -2,8 +2,12 @@
 // see [1] https://jeelabs.org/ref/STM32F1-RM0008.pdf
 
 struct Periph {  // [1] p.49-50
-    constexpr static uint32_t gpio = 0x40010800U;
-    constexpr static uint32_t rcc  = 0x40021000U;
+    constexpr static uint32_t rtc   = 0x40002800;
+    constexpr static uint32_t iwdg  = 0x40003000;
+    constexpr static uint32_t pwr   = 0x40007000;
+    constexpr static uint32_t gpio  = 0x40010800;
+    constexpr static uint32_t rcc   = 0x40021000;
+    constexpr static uint32_t flash = 0x40022000;
 };
 
 // interrupt vector table in ram
@@ -264,10 +268,9 @@ RingBuffer<NTX> UartBufDev<TX,RX,NTX,NRX>::xmit;
 // system clock
 
 static void enableClkAt72mhz () {  // [1] p.49
-    constexpr uint32_t rcc   = 0x40021000;
-    constexpr uint32_t flash = 0x40022000;
+    constexpr uint32_t rcc   = Periph::rcc;
 
-    MMIO32(flash + 0x00) = 0x12; // flash acr, two wait states
+    MMIO32(Periph::flash + 0x00) = 0x12; // flash acr, two wait states
     MMIO32(rcc + 0x00) |= (1<<16); // rcc cr, set HSEON
     while ((MMIO32(rcc + 0x00) & (1<<17)) == 0) ; // wait for HSERDY
     // 8 MHz xtal src, pll 9x, pclk1 = hclk/2, adcpre = pclk2/6 [1] pp.100
@@ -288,16 +291,14 @@ static int fullSpeedClock () {
 
 struct RTC {  // [1] pp.486
     constexpr static uint32_t bdcr = Periph::rcc + 0x20;
-    constexpr static uint32_t pwr  = 0x40007000;
-    constexpr static uint32_t rtc  = 0x40002800;
-    constexpr static uint32_t crl  = rtc + 0x04;
-    constexpr static uint32_t prll = rtc + 0x0C;
-    constexpr static uint32_t cnth = rtc + 0x18;
-    constexpr static uint32_t cntl = rtc + 0x1C;
+    constexpr static uint32_t crl  = Periph::rtc + 0x04;
+    constexpr static uint32_t prll = Periph::rtc + 0x0C;
+    constexpr static uint32_t cnth = Periph::rtc + 0x18;
+    constexpr static uint32_t cntl = Periph::rtc + 0x1C;
 
     static void init () {
         MMIO32(Periph::rcc + 0x18) |= (0b11 << 27);  // enable PWREN and BKPEN
-        MMIO32(pwr) |= (1 << 8);          // set DBP
+        MMIO32(Periph::pwr) |= (1 << 8);  // set DBP
         MMIO32(bdcr) |= (1 << 16);        // reset backup domain
         MMIO32(bdcr) &= ~(1 << 16);       // release backup domain
         MMIO32(bdcr) |= (1 << 0);         // LESON backup domain
@@ -384,11 +385,10 @@ struct SpiHw {  // [1] pp.742
 // independent watchdog
 
 struct Iwdg {  // [1] pp.495
-    constexpr static uint32_t iwdg = 0x40003000;
-    constexpr static uint32_t kr  = iwdg + 0x00;
-    constexpr static uint32_t pr  = iwdg + 0x04;
-    constexpr static uint32_t rlr = iwdg + 0x08;
-    constexpr static uint32_t sr  = iwdg + 0x0C;
+    constexpr static uint32_t kr  = Periph::iwdg + 0x00;
+    constexpr static uint32_t pr  = Periph::iwdg + 0x04;
+    constexpr static uint32_t rlr = Periph::iwdg + 0x08;
+    constexpr static uint32_t sr  = Periph::iwdg + 0x0C;
 
     Iwdg (int rate =7) {
         while (sr & (1<<0)) ;  // wait until !PVU
@@ -405,11 +405,10 @@ struct Iwdg {  // [1] pp.495
 // flash memory writing and erasing
 
 struct Flash {
-    constexpr static uint32_t base = 0x40022000;
-    constexpr static uint32_t keyr = base + 0x04;
-    constexpr static uint32_t sr   = base + 0x0C;
-    constexpr static uint32_t cr   = base + 0x10;
-    constexpr static uint32_t ar   = base + 0x14;
+    constexpr static uint32_t keyr = Periph::flash + 0x04;
+    constexpr static uint32_t sr   = Periph::flash + 0x0C;
+    constexpr static uint32_t cr   = Periph::flash + 0x10;
+    constexpr static uint32_t ar   = Periph::flash + 0x14;
 
     static void write16 (void* addr, uint16_t val) {
         if (*(uint16_t*) addr != 0xFFFF)
