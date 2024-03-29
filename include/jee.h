c5cdf812d4ec0fbdfca9e6d78016765ef8e6ff70
Jean-Claude Wippler <jc@wippler.nl>
2024-03-29 09:02:15 +0100

    oops

--- include/jee.h
+++ include/jee.h
@@ -6,10 +6,7 @@
 #include <cstdlib>
 #include <cstring>
 
-//CG3 svd defines
-#define STM32   1
-#define STM32G4 1
-#define SVDNAME "STM32G431xx"
+//CG: svd defines
 
 #ifdef NASSERT
 #define assert(x) ((void) 0) // don't evaluate x (i.e. prevent side-effects)
@@ -21,7 +18,7 @@
 namespace jeeh {
 
 //CG1 version
-constexpr auto VERSION = "714fbcf";
+constexpr auto VERSION = "<stripped>";
 
 #include "arch/sys.h"
 
