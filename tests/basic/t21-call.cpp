1943e746320f02b51eca6139105e3222f1f0da54
Jean-Claude Wippler <jc@wippler.nl>
2024-04-11 16:38:10 +0200

    t21 now triggers the same error as the sys::call in net

--- tests/basic/t21-call.cpp
+++ tests/basic/t21-call.cpp
@@ -3,21 +3,66 @@ using namespace jeeh;
 #include "test.h"
 
 struct One : Task {
+    Message* orig;
     Message timer { '@', 'T', 10 };
+    Message child { 2, 'X' };
+    uint8_t state =0;
+
+    // <0> send <1> reply
+    // <2> send <3> reply
+    // <4> call <5> reply
+    // <6> wait <7> reply
 
     void process (Message& msg) override {
-        logf("20 %d %c", msg.mDst, msg.mTag);
-        switch (msg.mTag) {
-            case 'I':
-                logf("21");
-                sys::call(timer);
-                logf("22");
+        logf("20 s%d %p d%d t%c", state, &msg, msg.mDst, msg.mTag);
+        switch (state++) {
+            case 0:
+            case 2:
+                assert(msg.mTag <= 'B');
+                orig = &msg;
+                sys::send(timer);
                 break;
-            case 'T':
-                logf("23");
-                sys::send(msg);
-                logf("24");
+            case 4:
+                assert(msg.mTag == 'C');
+//logf("21");
+                sys::call(child); // nested task call, see main @13
+//logf("22");
                 break;
+            case 6:
+                assert(msg.mTag == 'D');
+                sys::wait(30); // nested driver call
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                sys::send(*orig);
+                break;
+            default:
+                fail();
+        }
+    }
+};
+
+struct Two : Task {
+    Message timer { '@', 'T', 20 };
+    Message* orig;
+    uint8_t state =10;
+
+    // <10> send T <11> reply
+
+    void process (Message& msg) override {
+        logf("30 s%d %p d%d t%c", state, &msg, msg.mDst, msg.mTag);
+        switch (state++) {
+            case 10:
+                orig = &msg;
+                sys::send(timer);
+                break;
+            case 11:
+                sys::send(*orig);
+                break;
+            default:
+                fail();
         }
     }
 };
@@ -26,10 +71,35 @@ int main () {
     Tester t;
 
     One one;
+    Two two;
+    assert(one.mTag == 1);
+    assert(two.mTag == 2);
+logf("one %p t %p c %p two %p t %p",
+        (Message*) &one, &one.timer, &one.child,
+        (Message*) &two, &two.timer);
+
     sys::wait(0); // start Ticker
+    logf("10");
 
-    Message m { one.mTag, 'I' };
+    Message m { 1, 'A' };
     sys::send(m);
+    logf("11 %c", m.mTag);
 
-    sys::recv();
+    auto& r = sys::recv();
+    logf("12 %c", m.mTag);
+    assert(&r == &m);
+
+    m.mTag = 'B';
+    sys::call(m);
+    logf("13 %c", m.mTag);
+
+    m.mTag = 'C';
+    sys::call(m);
+    logf("14 %c", m.mTag);
+
+    m.mTag = 'D';
+    sys::call(m);
+    logf("15 %c", m.mTag);
+
+fail();
 }
