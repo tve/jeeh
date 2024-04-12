e2e7d661b20c92e196c4063b8c2f00d1ecaab62c
Jean-Claude Wippler <jc@wippler.nl>
2024-04-12 10:54:36 +0200

    add debug print

--- src/sys.cpp
+++ src/sys.cpp
@@ -401,8 +401,12 @@ Message& sys::recv () {
 void sys::call (Message& msg) {
     auto& th = context();
     th.block = &msg;
+logf("C1 %p d%d t%c", &msg, msg.mDst, msg.mTag);
     send(msg);
-    [[maybe_unused]] auto& r = recv(); assert(&r == &msg);
+logf("C2 %p d%d t%c", &msg, msg.mDst, msg.mTag);
+    [[maybe_unused]] auto& r = recv();
+logf("C3 %p d%d t%c", &r, r.mDst, msg.mTag);
+    assert(&r == &msg);
     th.block = nullptr;
 }
 
