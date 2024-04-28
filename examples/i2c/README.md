9c70298d56eb7e60bd735635f95546082663a8f1
Jean-Claude Wippler <jc@wippler.nl>
2024-04-28 22:04:03 +0200

    add 9-DOF I2C sensor readout demo

---
+++ examples/i2c/README.md
@@ -0,0 +1,15 @@
+# I2C sensor readout on a Nucleo-32 L432KC
+
+This uses a "9DOF Stick", attached to D11 and D12 (PB4 and PB5).
+
+Sample output:
+
+    HMC5883 compass: xyz = -397 -252 473
+    HMC5883 compass: xyz = -397 -252 473
+    HMC5883 compass: xyz = -397 -252 473
+    ADXL345 accel:   xyz = -1 -252 -15
+    ADXL345 accel:   xyz = -2 -264 -16
+    ADXL345 accel:   xyz = 0 -265 -14
+    ITG3200 gyro:    xyz = -4 31 5
+    ITG3200 gyro:    xyz = -5 31 1
+    ITG3200 gyro:    xyz = -5 32 2
--- examples/i2c/main.cpp
+++ examples/i2c/main.cpp
@@ -31,6 +31,7 @@ extern "C" int _write (int, char* ptr, int len) {
 int main () {
     hardFaulter = hardFaultHandler;
     fastClock();
+    cycles::init();
 
     // the UART config comes from platformio.ini and is defined in defs.h
     uart.init(UART_PINS, 115'200, { UART_NAME.ADDR, ena::UART_NAME,
@@ -44,6 +45,47 @@ int main () {
     printf("SDA=PB5, SCL=PB4\n");
     i2c.init("B5,B4");
     i2c.detect(); // look for audio codec
+
+    uint8_t buf [6];
+
+    for (auto i = 0; i < 3; ++i) {
+        sys::wait(50);
+        i2c.readRegs(HMC5883, 3, buf, sizeof buf);
+
+        auto x = (int16_t) ((buf[0] << 8) | buf[1]);
+        auto y = (int16_t) ((buf[2] << 8) | buf[3]);
+        auto z = (int16_t) ((buf[4] << 8) | buf[5]);
+        printf("HMC5883 compass: xyz = %d %d %d\n", x, y, z);
+    }
+
+    i2c.writeReg(ADXL345, 0x2D, 0x08); sys::wait(10); // Measurement mode
+    i2c.writeReg(ADXL345, 0x31, 0x08); sys::wait(10); // full resolution
+    i2c.writeReg(ADXL345, 0x2C, 0x09); sys::wait(10); // 50 Hz rate
+
+    for (auto i = 0; i < 3; ++i) {
+        sys::wait(50);
+        i2c.readRegs(ADXL345, 0x32, buf, sizeof buf);
+
+        auto x = ((int8_t) buf[1] << 8) | buf[0];
+        auto y = ((int8_t) buf[3] << 8) | buf[2];
+        auto z = ((int8_t) buf[5] << 8) | buf[4];
+        printf("ADXL345 accel:   xyz = %d %d %d\n", x, y, z);
+    }
+
+    i2c.writeReg(ITG3200, 0x3E, 0x80); sys::wait(10); // full-scale range, 42 Hz
+    i2c.writeReg(ITG3200, 0x16, 0x1B); sys::wait(10); // DLPF_CFG=3, FS_SEL=3
+    i2c.writeReg(ITG3200, 0x15, 0x0A); sys::wait(10); // SMPLRT_DIV = 10 (50Hz)
+    i2c.writeReg(ITG3200, 0x3E, 0x03); sys::wait(10); // PLL with z gyro ref
+
+    for (auto i = 0; i < 3; ++i) {
+        sys::wait(50);
+        i2c.readRegs(ITG3200, 0x1D, buf, sizeof buf);
+
+        auto x = (int16_t) ((buf[0] << 8) | buf[1]);
+        auto y = (int16_t) ((buf[2] << 8) | buf[3]);
+        auto z = (int16_t) ((buf[4] << 8) | buf[5]);
+        printf("ITG3200 gyro:    xyz = %d %d %d\n", x, y, z);
+    }
 
     while (true) {
         sys::wait(250);
