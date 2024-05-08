43e41e5a5ea6ec59d73c6d3661486a82549bfa9a
Jean-Claude Wippler <jc@wippler.nl>
2024-05-08 22:37:54 +0200

    latest test run results

--- tests/basic/defs.h
+++ tests/basic/defs.h
@@ -1,13 +1,13 @@
 //CG2 board exti
-#define EXT_IN  "A10"
-#define EXT_OUT "A9"
+#define EXT_IN  "A1"
+#define EXT_OUT "A0"
 
 //CG1 board leds
-#define LED  "B8"
+#define LED  "A5"
 
 //CG[ board uart
-#define UART_NAME  USART1
-#define UART_PINS  "A9:7,A10"
-#define UART_FREQ  150
-#define UART_CONF  Irq::DMA1_CH1,Irq::DMA1_CH2,1-1,1-1,2-1,25,24
+#define UART_NAME  UART4
+#define UART_PINS  "A0:8,A1"
+#define UART_FREQ  80
+#define UART_CONF  Irq::DMA2_Channel3,Irq::DMA2_Channel5,2-1,3-1,5-1,2,2
 //CG]
