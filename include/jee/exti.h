9e41ca35aad00554c609e29d370054effe0bea95
Jean-Claude Wippler <jc@wippler.nl>
2024-05-15 19:13:41 +0200

    verify all blink builds

--- include/jee/exti.h
+++ include/jee/exti.h
@@ -14,7 +14,7 @@ struct ExtIrq : Device, Chain {
     enum { NONE, RISE, FALL, BOTH };
 
     ExtIrq () : Device ('E') {
-#if !STM32WL
+#if !(STM32L0 | STM32WL)
         RCC(ena::SYSCFG, 1) = 1;
 #endif
 #if STM32G0 | STM32L0
--- src/stm32.cpp
+++ src/stm32.cpp
@@ -205,7 +205,7 @@ enum { BDCR=0x90 };
 #endif
 
 void init (bool lse) {
-#if !(STM32F3 | STM32F4 | STM32L4)
+#if !(STM32F3 | STM32F4 | STM32F7 | STM32L0 | STM32L4)
     RCC(ena::RTCAPB, 1) = 1;
 #endif
 #if !(STM32H7 | STM32WL)
