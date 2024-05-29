0e3a3e0b5e74da9432e1d69352fe0c0308bdbedb
Jean-Claude Wippler <jc@wippler.nl>
2024-05-29 16:28:31 +0200

    streamline for reuse in more apps

--- examples/hytiny/blink.cpp
+++ examples/hytiny/blink.cpp
@@ -1,40 +1,16 @@
-// Minimal LED blinker, using a busy loop as delay.
+// LED blinker, with RTC seconds sent to the UART console.
 
 #include <jee.h>
 #include <jee/hal.h>
 using namespace jeeh;
 #include "defs.h"
 
-Uart uart ('U');
-
-extern "C" int _write (int, char* ptr, int len) {
-    Message m { 'U', 'W', (uint16_t) len, (uint8_t*) ptr };
-    sys::call(m);
-    return len;
-}
-
 int main () {
-    fastClock();
-    uart.init(UART_PINS, 115'200, { UART_NAME.ADDR, ena::UART_NAME,
-                                    UART_FREQ, Irq::UART_NAME, UART_CONF });
-    printf("\n%s: blink @ %d MHz\n", SVDNAME, SystemCoreClock / 1'000'000);
-
-    rtc::init(false);
-
-    Pin led (LED);  // defined in platformio.ini
-    led.mode("P");  // push-pull output
+    initBoard("blink"); // in defs.h
 
     while (true) {
-        printf("%d\n", rtc::getSecs());
+        printf("%d\n", rtc::getSecs()); // F1 can't read sub-seconds from RTC
         led.toggle();
         sys::wait(500);
     }
 }
-
-void jeeh::fail (void const* a, char const* f, int n) {
-    printf("%s:%d - called at %p\n", f, n, a);
-    while (true) {}
-}
-
-void jeeh::LowPower::start (Message&) {}
-void jeeh::LowPower::finish () {}
--- examples/hytiny/defs.h
+++ examples/hytiny/defs.h
@@ -7,3 +7,31 @@
 #define UART_FREQ  72
 #define UART_CONF  Irq::DMA1_Channel4,Irq::DMA1_Channel5,1-1,4-1,5-1,0,0
 //CG]
+
+constexpr Pin led (LED);
+
+void jeeh::LowPower::start (Message&) {}
+void jeeh::LowPower::finish () {}
+
+void jeeh::fail (void const* a, char const* f, int n) {
+    printf("%s:%d - called at %p\n", f, n, a);
+    while (true) {}
+}
+
+uint8_t initBoard (char const* appName) {
+    fastClock();
+    led.mode("P");
+    rtc::init(false);
+
+    static Uart uart ('U');
+    uart.init(UART_PINS, 115'200, { UART_NAME.ADDR, ena::UART_NAME,
+                                    UART_FREQ, Irq::UART_NAME, UART_CONF });
+    printf("\n%s: %s @ %d MHz\n", SVDNAME, appName, SystemCoreClock / 1'000'000);
+    return uart.dId;
+}
+
+extern "C" int _write (int, char* ptr, int len) {
+    Message m { 'U', 'W', (uint16_t) len, (uint8_t*) ptr };
+    sys::call(m);
+    return len;
+}
