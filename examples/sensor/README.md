0a99b4eea1caf5f3c25e606722a2013648131b06
Jean-Claude Wippler <jc@wippler.nl>
2024-05-30 23:48:34 +0200

    add L031 as sensor node w/ RFM69

---
+++ examples/sensor/README.md
@@ -0,0 +1,1 @@
+**Low-power explorations using a Nucleo-L031K6 as sensor node w/ RFM69.**
---
+++ examples/sensor/blink.cpp
@@ -0,0 +1,22 @@
+// Minimal LED blinker, using a busy loop as delay.
+
+#include <jee.h>
+using namespace jeeh;
+#include "defs.h"
+
+int main () {
+    rtc::init();
+
+    Pin led (LED);  // defined in platformio.ini
+    led.mode("P");  // push-pull output
+
+    while (true) {
+        led.toggle();
+        //rtc::shortSleep(100, sys::STOP2);
+        sys::wait(250);
+    }
+}
+
+void jeeh::fail(void const*, char const*, int) { while (true) {} }
+void LowPower::start (Message&) {}
+void LowPower::finish() {}
