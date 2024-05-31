c9c8fa8f6d3f76ffa58290fe5a21d89b99d5f8fb
Jean-Claude Wippler <jc@wippler.nl>
2024-05-31 20:59:08 +0200

    add uart

--- examples/wsn/blink.cpp
+++ examples/wsn/blink.cpp
@@ -1,16 +1,15 @@
 // Minimal LED blinker, using a busy loop as delay.
 
 #include <jee.h>
+#include <jee/hal.h>
 using namespace jeeh;
 #include "defs.h"
 
 int main () {
-    rtc::init();
-
-    Pin led (LED);  // defined in platformio.ini
-    led.mode("P");  // push-pull output
+    initBoard();
 
     while (true) {
+        logf("%d", rtc::getSecs());
         led.toggle();
         //rtc::shortSleep(100, sys::STOP2);
         sys::wait(250);
--- examples/wsn/defs.h
+++ examples/wsn/defs.h
@@ -2,8 +2,26 @@
 #define LED  "B3"
 
 //CG[ board uart
-#define UART_NAME  USART1
-#define UART_PINS  "A9:7,A10"
-#define UART_FREQ  72
-#define UART_CONF  Irq::DMA1_Channel4,Irq::DMA1_Channel5,1-1,4-1,5-1,0,0
+#define UART_NAME  USART2
+#define UART_PINS  "A9:4,A10"
+#define UART_FREQ  32
+#define UART_CONF  Irq::DMA1_Channel4,Irq::DMA1_Channel5,1-1,4-1,5-1,4,4
 //CG]
+
+constexpr Pin led (LED);  // defined in platformio.ini
+
+Uart uart ('U');
+
+void logWriter (void const* ptr, size_t len) {
+    Message m { 'U', 'W', (uint16_t) len, (uint8_t*) ptr };
+    sys::call(m);
+}
+
+void initBoard () {
+    rtc::init();
+
+    led.mode("P");  // push-pull output
+
+    uart.init(UART_PINS, 115'200, { UART_NAME.ADDR, ena::UART_NAME,
+                                    UART_FREQ, Irq::UART_NAME, UART_CONF });
+}
