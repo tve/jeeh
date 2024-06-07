4e543d38ea8398d14f545639c13c7df35f631758
Jean-Claude Wippler <jc@wippler.nl>
2024-06-07 15:07:43 +0200

    fix build errors

--- examples/z474/blink.cpp
+++ examples/z474/blink.cpp
@@ -1,11 +1,11 @@
 // Minimal LED blinker, using a busy loop as delay.
 
 #include <jee.h>
+#include <jee/hal.h>
 using namespace jeeh;
 #include "defs.h"
 
 int main () {
-    Pin led (LED);  // defined in platformio.ini
     led.mode("P");  // push-pull output
 
     while (true) {
--- examples/z474/hello.cpp
+++ examples/z474/hello.cpp
@@ -6,7 +6,7 @@ using namespace jeeh;
 #include "defs.h"
 
 int main () {
-    initBoard();
+    initBoard("hello");
 
     while (true) {
         led.toggle();
