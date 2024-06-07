19f25e3cf67fc89917b4d76d2b2edd1994fbc376
Jean-Claude Wippler <jc@wippler.nl>
2024-06-07 15:22:09 +0200

    reorg ... pfff

--- examples/z474/alive.cpp
+++ examples/z474/alive.cpp
@@ -5,31 +5,30 @@
 using namespace jeeh;
 #include "defs.h"
 
+struct Kicker {
+    Message timer { '@', 'T' };
+
+    Kicker () { trigger(timer); }
+
+    void trigger (Message&) {
+        logf("kick @ %d ms", rtc::getDate().todMillis() % 10'000);
+        dog::kick();
+
+        static int ms = 1000;
+        timer.mLen = ms; // timeout
+        ms += 500;
+
+        sys::send(timer.setCallback(this, &Kicker::trigger));
+    }
+};
+
 int main () {
     initBoard("alive");
 
     logf("reset %d", dog::resetCause());
 
-    dog::init(2); // will fire approx. every 2 secs
-
-    struct Kicker {
-        Message timer { '@', 'T' };
-
-        Kicker () { trigger(timer); } // start the cycle
-
-        void trigger (Message&) {
-            logf("kick @ %d ms", rtc::getDate().todMillis() % 10'000);
-            dog::kick();
-
-            static int ms = 1000;
-            timer.mLen = ms; // timeout
-            ms += 500;
-
-            sys::send(timer.setCallback(this, &Kicker::trigger));
-        }
-    };
-
-    Kicker kicker;
+    dog::init(2);  // will fire approx. every 2 secs
+    Kicker kicker; // start the background timer
 
     int i = 0;
     while (true) {
