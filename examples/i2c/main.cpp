5632f650bc01adcb769d3fba6a52eb01a50eab43
Jean-Claude Wippler <jc@wippler.nl>
2024-06-15 23:54:28 +0200

    clean up and reduce I2cGpio delays

--- examples/i2c/main.cpp
+++ examples/i2c/main.cpp
@@ -26,74 +26,80 @@ int main () {
     uart.init(UART_PINS, 115'200, { UART_NAME.ADDR, ena::UART_NAME,
                                     UART_FREQ, Irq::UART_NAME, UART_CONF });
 
-    printf("???\r%s @ %d MHz\n", SVDNAME, SystemCoreClock / 1'000'000);
+    printf("\n%s @ %d MHz\n", SVDNAME, SystemCoreClock / 1'000'000);
     led.mode("P"); // push-pull output
 
     I2cGpio i2c;
     printf("SCL=PB6, SDA=PB7\n");
-    i2c.init("B6,B7");
+    i2c.init("B6,B7", 1);
     i2c.detect(); // look for audio codec
 
     uint8_t buf [6];
 
-    i2c.writeReg(HMC5883, 0, 0x18); sys::wait(10); // 75 Hz
-    i2c.writeReg(HMC5883, 1, 0x40); sys::wait(10); // +/- 1.9 Ga
-    i2c.writeReg(HMC5883, 2, 0x00); sys::wait(10); // continuous
+    i2c.writeReg(HMC5883, 0, 0x18); // 75 Hz
+    i2c.writeReg(HMC5883, 1, 0x40); // +/- 1.9 Ga
+    i2c.writeReg(HMC5883, 2, 0x00); // continuous
 
-    for (auto i = 0; i < 3; ++i) {
-        sys::wait(50);
+    i2c.writeReg(ADXL345, 0x2D, 0x08); // Measurement mode
+    i2c.writeReg(ADXL345, 0x31, 0x08); // full resolution
+    i2c.writeReg(ADXL345, 0x2C, 0x09); // 50 Hz rate
+
+    i2c.writeReg(ITG3200, 0x3E, 0x80); // full-scale range, 42 Hz
+    i2c.writeReg(ITG3200, 0x16, 0x1B); // DLPF_CFG=3, FS_SEL=3
+    i2c.writeReg(ITG3200, 0x15, 0x0A); // SMPLRT_DIV = 10 (50Hz)
+    i2c.writeReg(ITG3200, 0x3E, 0x03); // PLL with z gyro ref
+
+    for (auto i = 0; i < 4; ++i) {
+        sys::wait(25);
         i2c.readRegs(HMC5883, 3, buf, sizeof buf);
 
         auto x = (int16_t) ((buf[0] << 8) | buf[1]);
         auto y = (int16_t) ((buf[2] << 8) | buf[3]);
         auto z = (int16_t) ((buf[4] << 8) | buf[5]);
-        printf("HMC5883 compass: xyz = %d %d %d\n", x, y, z);
+        if (i > 0)
+            printf("HMC5883 compass: xyz = %4d %4d %4d\n", x, y, z);
     }
 
-    i2c.writeReg(ADXL345, 0x2D, 0x08); sys::wait(10); // Measurement mode
-    i2c.writeReg(ADXL345, 0x31, 0x08); sys::wait(10); // full resolution
-    i2c.writeReg(ADXL345, 0x2C, 0x09); sys::wait(10); // 50 Hz rate
-
-    for (auto i = 0; i < 3; ++i) {
-        sys::wait(50);
+    for (auto i = 0; i < 4; ++i) {
+        sys::wait(25);
         i2c.readRegs(ADXL345, 0x32, buf, sizeof buf);
 
         auto x = ((int8_t) buf[1] << 8) | buf[0];
         auto y = ((int8_t) buf[3] << 8) | buf[2];
         auto z = ((int8_t) buf[5] << 8) | buf[4];
-        printf("ADXL345 accel:   xyz = %d %d %d\n", x, y, z);
+        if (i > 0)
+            printf("ADXL345 accel:   xyz = %4d %4d %4d\n", x, y, z);
     }
 
-    i2c.writeReg(ITG3200, 0x3E, 0x80); sys::wait(10); // full-scale range, 42 Hz
-    i2c.writeReg(ITG3200, 0x16, 0x1B); sys::wait(10); // DLPF_CFG=3, FS_SEL=3
-    i2c.writeReg(ITG3200, 0x15, 0x0A); sys::wait(10); // SMPLRT_DIV = 10 (50Hz)
-    i2c.writeReg(ITG3200, 0x3E, 0x03); sys::wait(10); // PLL with z gyro ref
-
-    for (auto i = 0; i < 3; ++i) {
-        sys::wait(50);
+    for (auto i = 0; i < 4; ++i) {
+        sys::wait(25);
         i2c.readRegs(ITG3200, 0x1D, buf, sizeof buf);
 
         auto x = (int16_t) ((buf[0] << 8) | buf[1]);
         auto y = (int16_t) ((buf[2] << 8) | buf[3]);
         auto z = (int16_t) ((buf[4] << 8) | buf[5]);
-        printf("ITG3200 gyro:    xyz = %d %d %d\n", x, y, z);
+        if (i > 0)
+            printf("ITG3200 gyro:    xyz = %4d %4d %4d\n", x, y, z);
     }
 
-    SSD1306 oled (i2c);
-    oled.init();
+    SSD1306 oled1 (i2c);
+    oled1.init();
 
     // display a trivial pattern, just to verify that it works
-    static uint8_t data [] = {
+    static uint8_t data1 [] = {
         255,0,255,0,255,0,255,0,255,0,255,0,255,0,255,0,255,0,255
     };
-    oled.copyBand (  0,  0, data, sizeof data);
-    oled.copyBand (  1,  8, data, sizeof data);
-    oled.copyBand (  2, 16, data, sizeof data);
-    oled.copyBand (  3, 24, data, sizeof data);
-    oled.copyBand (100,  0, data, sizeof data);
-    oled.copyBand (101,  8, data, sizeof data);
-    oled.copyBand (102, 16, data, sizeof data);
-    oled.copyBand (103, 24, data, sizeof data);
+    auto t1 = cycles::count();
+    oled1.copyBand (  0,  0, data1, sizeof data1);
+    oled1.copyBand (  1,  8, data1, sizeof data1);
+    oled1.copyBand (  2, 16, data1, sizeof data1);
+    oled1.copyBand (  3, 24, data1, sizeof data1);
+    oled1.copyBand (100,  0, data1, sizeof data1);
+    oled1.copyBand (101,  8, data1, sizeof data1);
+    oled1.copyBand (102, 16, data1, sizeof data1);
+    oled1.copyBand (103, 24, data1, sizeof data1);
+    t1 = cycles::count() - t1;
+    printf("oled1 %d µs\n", t1 / (SystemCoreClock/1'000'000));
 
     SSD1306<decltype(i2c), true, 0x3D> oled2 (i2c);
     oled2.init();
@@ -102,6 +108,7 @@ int main () {
     static uint8_t data2 [] = {
         255,0,255,0,255,0,255,0,255,0,255,0,255,0,255,0,255,0,255
     };
+    auto t2 = cycles::count();
     oled2.copyBand (  0,  0, data2, sizeof data2);
     oled2.copyBand (  1,  8, data2, sizeof data2);
     oled2.copyBand (  2, 16, data2, sizeof data2);
@@ -118,6 +125,8 @@ int main () {
     oled2.copyBand (105, 40, data2, sizeof data2);
     oled2.copyBand (106, 48, data2, sizeof data2);
     oled2.copyBand (107, 56, data2, sizeof data2);
+    t2 = cycles::count() - t2;
+    printf("oled2 %d µs\n", t2 / (SystemCoreClock/1'000'000));
 
     while (true) {
         sys::wait(250);
