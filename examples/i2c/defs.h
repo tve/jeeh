7c999debaf1663ecf2aec51e20ce5a84097e55d5
Jean-Claude Wippler <jc@wippler.nl>
2024-07-03 00:07:08 +0200

    cleanup

--- examples/i2c/defs.h
+++ examples/i2c/defs.h
@@ -1,7 +1,7 @@
 // Lines with "CG" control the code-generated parts of this file.
 
 //CG1 pio
-#define PIOENV  "rtc-gpio"
+#define PIOENV  "rtc-call"
 
 //CG1 board leds
 #define LED  "B3"
@@ -26,7 +26,7 @@ inline Uart console ('U');
 //CG]
 
 //CG1 board mode
-#define MODE_GPIO 1
+#define MODE_CALL 1
 
 #if MODE_GPIO
 i2c::Gpio i2cBus;
--- examples/i2c/rtc.cpp
+++ examples/i2c/rtc.cpp
@@ -1,27 +1,23 @@
-// Simple I2C sensor readout example.
+// Read out the DS3231 RTC module (and optionally also set it).
 
 #include <jee.h>
 #include <jee/hal.h>
 using namespace jeeh;
 #include "defs.h"
 
-uint8_t bcd2bin (uint8_t b) { return b - 6 * (b/16); }
-uint8_t bin2bcd (uint8_t b) { return b + 6 * (b/10); }
+uint8_t bcd2bin (uint8_t b) { return b - 6*(b/16); }
+uint8_t bin2bcd (uint8_t b) { return b + 6*(b/10); }
 
 int main () {
     initBoard();
-    i2cBus.init(I2C_PINS, 400);
+    i2cBus.init(I2C_PINS, 100);
     i2c::Dev ds3231 {i2cBus, 0x68};
 
     if (0) { // set the date
         DateTime now; // the time this code was compiled, that is ...
-        uint8_t buf [] = { bin2bcd(now.ss),
-                           bin2bcd(now.mm),
-                           bin2bcd(now.hh),
+        uint8_t buf [] = { bin2bcd(now.ss), bin2bcd(now.mm), bin2bcd(now.hh),
                            1, // day of week, not used
-                           bin2bcd(now.dy),
-                           bin2bcd(now.mo),
-                           bin2bcd(now.yr) };
+                           bin2bcd(now.dy), bin2bcd(now.mo), bin2bcd(now.yr) };
         ds3231.write(0x00, buf, sizeof buf);
     }
 
@@ -29,12 +25,8 @@ int main () {
     ds3231.read(0x00, buf, sizeof buf);
     logDump(buf);
 
-    DateTime dt { bcd2bin(buf[6]),
-                  bcd2bin(buf[5]),
-                  bcd2bin(buf[4]),
-                  bcd2bin(buf[2]),
-                  bcd2bin(buf[1]),
-                  bcd2bin(buf[0]) };
+    DateTime dt { bcd2bin(buf[6]), bcd2bin(buf[5]), bcd2bin(buf[4]),
+                  bcd2bin(buf[2]), bcd2bin(buf[1]), bcd2bin(buf[0]) };
 
     logf("20%02d/%02d/%02d %02d:%02d:%02d",
             dt.yr, dt.mo, dt.dy, dt.hh, dt.mm, dt.ss);
