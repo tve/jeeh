18f15a47b6037ff4bbb1149a807a73e642bf7625
Jean-Claude Wippler <jc@wippler.nl>
2024-07-05 01:12:20 +0200

    add P1 meter decoding

--- examples/jnzero/recv.cpp
+++ examples/jnzero/recv.cpp
@@ -1,4 +1,4 @@
-// Dump received RFM69 packets to the serial port.
+// Dump received RFM69 power metering packets to the serial port.
 
 #include <jee.h>
 #include <jee/hal.h>
@@ -27,9 +27,29 @@ int main () {
         auto n = rf.receive(rxBuf, sizeof rxBuf);
         if (n > 0) {
             auto dt = rtc::getDate();
-            logf("rssi %d lna %d afc %d @ %02d:%02d:%02d",
-                    rf.rssi, rf.lna, rf.afc, dt.hh, dt.mm, dt.ss);
-            logDump(rxBuf, n);
+            //logf("rssi %d lna %d afc %d @ %02d:%02d:%02d",
+            //        rf.rssi, rf.lna, rf.afc, dt.hh, dt.mm, dt.ss);
+            //logDump(rxBuf, n);
+
+            int vals [12];
+            auto i = 2, j = 0;
+            while (i < n && j < 12) {
+                auto v = 0;
+                do
+                    v = (v<<7) ^ rxBuf[i];
+                while (rxBuf[i++] < 0x80);
+                v ^= 0x80;
+                vals[j++] = v;
+                //logf(" %d", v);
+            }
+
+            if (vals[0] == 1 && j == 11) // it's a packet from p1mon.cpp
+                // 11 values: 4x Wh, tariff, 2x 10W, gas, liter, valve
+                // kwh: consume: normal low, produce: normal low
+                logf("%02d:%02d:%02d    %d %d %d %d %d %d %d %d %d %d",
+                        dt.hh, dt.mm, dt.ss,
+                        vals[1], vals[2], vals[3], vals[4], vals[5],
+                        vals[6], vals[7], vals[8], vals[9], vals[10]);
         }
         sys::wait(1000);
     }
