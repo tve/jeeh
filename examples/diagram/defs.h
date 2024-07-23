2afbc3ee90fccc889615c26fda591506f0d8d6ff
Jean-Claude Wippler <jc@wippler.nl>
2024-07-23 19:31:50 +0200

    add exti demo, untested

--- examples/diagram/defs.h
+++ examples/diagram/defs.h
@@ -1,7 +1,7 @@
 // Lines with "CG" control the code-generated parts of this file.
 
 //CG1 pio
-#define PIOENV  "rctimer"
+#define PIOENV  "exti"
 
 //CG1 board leds
 #define LED  "B3"
---
+++ examples/diagram/exti.cpp
@@ -0,0 +1,76 @@
+// Event-driven EXTI example with external cap, using a safety timeout.
+
+#include <jee.h>
+#include <jee/hal.h>
+using namespace jeeh;
+#include "defs.h"
+
+struct Example {
+    ExtIrq exti;
+    Pin rc {"A12"}; // this pin has ≈800 nF capacitance tied to ground
+
+    Message timer { '@', 'T' };
+    Message rcPin { exti.dId, 'A', 12, (uint8_t*) ExtIrq::BOTH };
+
+    Example () {
+        timer.setCallback(this, &Example::onTimeout);
+        rcPin.setCallback(this, &Example::onPinChange);
+        start('I');
+    }
+
+    void start (char type) {
+        assert(!timer.inUse());
+        assert(!rcPin.inUse());
+
+        sys::wait(100);
+        logf("%c %d", type, cycles::millis());
+
+        Tracer<9> pt;
+        led.toggle();
+        rc.mode(rc ? "D": "U"); // switch to pull-down or pull-up
+
+        timer.mLen = 30;
+        sys::send(timer); // generate a timeout in case pin change is missing
+
+        sys::send(rcPin); // trigger on pin change, some 15..25 ms from now
+    }
+
+    void onPinChange (Message&) {
+        sys::drop(timer, '@');
+
+        rc = +rc;     // force output to same state as currently read
+        rc.mode("P"); // ... then enable push-pull mode
+
+        static auto i = 0;
+        logf("%d", ++i); // uses blocking polled I/O
+        logf("T %d", cycles::millis());
+
+        start('+');
+    }
+
+    void onTimeout (Message &) {
+        sys::drop(rcPin, exti.dId);
+        start('T');
+    }
+};
+
+int main () {
+    initBoard();
+    Pin::config("B4:V15"); // EVENTOUT
+
+    Example activity;
+
+    while (true) {
+        Tracer<10> pt;
+        sys::recv();
+    }
+}
+
+uint8_t jeeh::lowestPower (uint8_t power, uint16_t) {
+    Tracer<11> pt;
+    return sys::STOP2;
+}
+
+void jeeh::resumePower () {
+    Tracer<12> pt;
+}
