de0ffb4eb296b456f31e592c1cd11e49fc23ef5b
Jean-Claude Wippler <jc@wippler.nl>
2024-09-05 20:32:53 +0200

    a new-style device is now a Worker+Trigger object

--- examples/dcf77/worker.cpp
+++ examples/dcf77/worker.cpp
@@ -8,13 +8,16 @@ using namespace jeeh;
 
 struct Event {
     uint32_t eDst :8;
-    uint32_t eTag :24;
+    uint32_t eTag :8;
+    uint32_t eVal :16;
 
-    Event (int dst, int tag =0) : eDst (dst), eTag (tag) {}
+    Event (uint8_t dst =0, uint8_t tag =0, uint16_t val =0)
+        : eDst (dst), eTag (tag), eVal (val) {}
 };
 
 struct Worker {
     uint8_t wId =++wSeq;
+    uint8_t head =0;
 
     Worker () {
         assert(wId < sizeof workers / sizeof *workers);
@@ -22,64 +25,119 @@ struct Worker {
     }
 
     ~Worker () {
-        workers[wId] = {};
+        workers[wId] = nullptr;
     }
 
-    virtual Event process (Event req, void* arg, Event reply) =0;
-
-    inline static void dispatch (Event req, void* arg ={}, Event reply =0) {
-        auto n = req.eDst;
-        assert(n < sizeof workers / sizeof *workers && workers[n] != nullptr);
-        workers[n]->process(req, arg, reply);
+    Event toSelf (uint8_t tag, uint16_t val =0) {
+        return { wId, tag, val };
     }
 
+    virtual Event process (Event req, Event reply, void* arg) =0;
+
+    static Worker& at (uint8_t id) {
+        assert(0 < id && id < sizeof workers / sizeof *workers);
+        assert(workers[id] != nullptr);
+        return *workers[id];
+    }
+
+    void pend (Event e) {
+        auto next = ++free;
+        // TODO pull(free);
+        pool[next] = Event (head, e.eTag, e.eVal);
+        head = next;
+    }
+
+    Event pull () {
+        assert(head != 0);
+        auto& curr = pool[head];
+        head = curr.eDst;
+        return Event (~0, curr.eTag, curr.eVal);
+    }
+
+    static inline Worker* current;
 private:
-    inline static uint8_t wSeq;
-    inline static Worker* workers [20];
+    static inline uint8_t wSeq;
+    static inline Worker* workers [20];
+
+    static constexpr auto NPOOL = 100;
+    static inline Event pool [NPOOL];
+    static inline uint8_t free;
 };
 
-struct Trigger {
+namespace jeeh::sys {
+    void Xsend (Event req, Event reply ={}, void* arg =nullptr) {
+        // FIXME this needs to POSTPONE if the priority is lower!
+        //  and pick up all pending events before resuming
+        Worker::at(req.eDst).process(req, reply, arg);
+    }
+}
+
+struct Trigger : Device { // TODO get rid of Device type
     uint8_t tId =++tSeq;
 
-    Trigger () {
+    Trigger () : Device ('A'+tId) {
         assert(tId < sizeof triggers / sizeof *triggers);
         triggers[tId] = this;
+    }
+
+    static Trigger& at (uint8_t id) {
+        assert(0 < id && id < sizeof triggers / sizeof *triggers);
+        assert(triggers[id] != nullptr);
+        return *triggers[id];
     }
 
     virtual void interrupt () =0;
 
 private:
-    inline static uint8_t tSeq;
-    inline static Trigger* triggers [20];
+    // don't use most of the Device logic, only its interrupt handling
+    void start (Message&) override { fail(); }
+    void finish () override { fail(); }
+    bool interrupt (int) override { interrupt(); return false; }
+
+    static inline uint8_t tSeq;
+    static inline Trigger* triggers [20];
 };
 
-struct Dcf77 : Worker {
-    Decoder d;
+struct Dcf77 : Worker, Trigger {
+    enum TAG { INIT, STEP };
 
-    void init () {
-        STK[0x4] = (SystemCoreClock/256) / 8 - 1; // reload value for ≈256 Hz
-        STK[0x8] = 0;
-        STK[0x0] = 0b011; // enable, clk/8 mode
-    }
+    Decoder d; // see decoder.h, needs to be called 256x per second
 
-    Event process (Event, void*, Event reply) override {
-        d.step(led);
+    Event process (Event request, Event reply, void*) override {
+        switch (request.eTag) {
+            case INIT:
+                // set up SysTick interrupts at ≈256 Hz
+                STK[0x4] = (SystemCoreClock/256) / 8 - 1;
+                STK[0x8] = 0;
+                STK[0x0] = 0b011; // enable, clk/8 mode
+                break;
+            case STEP:
+                led = request.eVal;
+                d.step(request.eVal);
+                break;
+            default:
+                fail(); // unknown request
+        }
         return reply;
     }
+
+    void interrupt () override {
+        sys::Xsend(toSelf(STEP, dcfData));
+    }
+
 };
 
 Dcf77 app;
 
 // this needs "-DMYSYSTICK" to disable JeeH's default SysTick handler
 extern "C" void SysTick_Handler () {
-    led = dcfData;
-    Worker::dispatch(app.wId);
+    app.interrupt(); // TODO tie this into Ticker iso of this demo app
 }
 
 int main() {
     initBoard();
 
-    app.init();
+    sys::Xsend(app.toSelf(app.INIT));
     while (true)
         asm ("wfi");
 }
