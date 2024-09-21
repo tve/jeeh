7dada2d6d13e68e7c683639ee26aa9324f1fc907
Jean-Claude Wippler <jc@wippler.nl>
2024-09-21 18:40:55 +0200

    start on the timer queue and pended events

--- builds/base/test/common.h
+++ builds/base/test/common.h
@@ -13,6 +13,15 @@ void jeeh::fail (void const*, char const*, int line) {
 
 void jeeh::hardFaultHandler (uint32_t*) { fail(); }
 
+// allow the use of printf
+extern "C" int putchar (int ch);
+
+extern "C" int _write (int, char* ptr, int len) {
+    for (auto i = 0; i < len; ++i)
+        putchar(ptr[i]);
+    return len;
+}
+
 extern void allTests ();
 
 int main () {
@@ -44,17 +53,31 @@ struct Worker {
     uint8_t head =0;
 
     Worker () {}
+    ~Worker () { workers[wId] = nullptr; }
 
     uint8_t init () {
-        assert(wId == 0);
-        wId =++wSeq;
-        assert(wId < sizeof workers / sizeof *workers);
-        workers[wId] = this;
+        if (workers[wId] == nullptr) {
+            wId =++wSeq;
+            assert(wId < sizeof workers / sizeof *workers);
+            workers[wId] = this;
+        }
         return wId;
     }
 
     static void send (Event evt, Event reply ={}, void* arg =nullptr) {
         assert(evt.eDst != 0);
+
+        auto irq = inIrq();
+        if (irq != 0 && irq != 14) { // postpone when called from an IRQ
+            assert(reply.eDst == 0 && arg == nullptr); // only replies allowed
+            at(evt.eDst).pend(evt);
+            return;
+        }
+
+        accept(evt, reply, arg);
+    }
+
+    static void accept (Event evt, Event reply ={}, void* arg =nullptr) {
         // TODO must postpone call when sending to a lower-priority worker!
         auto prev = current;
         current = &at(evt.eDst);
@@ -64,20 +87,56 @@ struct Worker {
             send(reply); // won't recurse again (i.e. can't reply to a reply)
     }
 
+    static void clearAll () {
+        memset(workers, 0, sizeof workers);
+        wSeq = 0;
+    }
+
     static Worker& at (uint8_t id) {
         assert(0 < id && id < sizeof workers / sizeof *workers);
         assert(workers[id] != nullptr);
         return *workers[id];
     }
 
+    static void dispatch () {
+        for (auto e : workers)
+            if (e != nullptr && e->head != 0)
+                accept(e->pull());
+    }
+
     static inline Worker* current;
 
 protected:
     virtual Event process (Event in, Event out, void* arg) =0;
+
 private:
+    static uint32_t inIrq () {
+        uint32_t ipsr;
+        asm ("mrs %0, ipsr" : "=r" (ipsr));
+        return ipsr;
+    }
+
+    void pend (Event e) {
+        auto next = ++free;
+        // TODO pull(free);
+        pool[next] = Event (head, e.eTag, e.eVal);
+        head = next;
+        SCB[0x04](28) = 1; // ICSR PENDSVSET
+    }
+
+    Event pull () {
+        assert(head != 0);
+        auto h = pool[head];
+        head = h.eDst;
+        h.eDst = wId;
+        return h;
+    }
+
     static inline uint8_t wSeq;
     static inline Worker* workers [20];
 
     static inline Event pool [100];
     static inline uint8_t free;
 };
+
+IRQ_HANDLER(PendSV, Worker::dispatch)
--- builds/base/test/test_tick/tick.cpp
+++ builds/base/test/test_tick/tick.cpp
@@ -3,13 +3,27 @@
 #include "../common.h"
 
 void setUp () {}
-void tearDown () {}
+void tearDown () { Worker::clearAll(); }
 
-class TickWorker : Worker {
+template< uint8_t MAX >
+class Ticker : Worker {
     Event process (Event in, Event out, void*) override {
         switch (in.eTag) {
             case TICK:
+                while (expired()) {
+                    auto h = head;
+                    head = links[h];
+                    send(timers[h]);
+                    timers[h].eVal = free;
+                    free = h;
+                }
                 break;
+            case RATE:
+                setRate(in.eVal);
+                break;
+            case DELAY:
+                add(in.eVal, out);
+                return {};
             default:
                 fail();
         }
@@ -23,11 +37,30 @@ class TickWorker : Worker {
         STK[0x0] = 0b011; // enable, clk/8 mode
     }
 
+    void add (uint16_t ms, Event out) {
+        out.eVal = ticks + ms; // deadline
+        auto slot = 0;
+        if (free != 0) {
+            slot = links[free];
+            free = timers[slot].eVal;
+        } else
+            slot = ++last;
+        links[slot] = head;
+        head = slot;
+        timers[head] = out;
+    }
+
+    bool expired () const {
+        return head != 0 && (uint16_t) (timers[head].eVal - ticks - 1) > 60000;
+    }
+
     uint8_t rate =0;
     volatile uint32_t ticks =0;
+    Event timers [MAX];
+    uint8_t links [MAX], head =0, free =0, last =0;
 
 public:
-    enum TAG { TICK };
+    enum TAG { TICK, RATE, DELAY };
 
     uint8_t init () {
         Worker::init();
@@ -37,7 +70,8 @@ public:
 
     void interrupt () {
         ticks += rate;
-        send({ wId, TICK, rate });
+        if (expired())
+            send({ wId, TICK });
     }
 
     uint32_t millis () const {
@@ -47,22 +81,89 @@ public:
                 return t + rate - (c*8)/(SystemCoreClock/1000);
             }
     }
+
+    void delay (uint16_t ms, Event reply) const {
+        assert(reply.eDst != 0);
+        send({ wId, DELAY, ms }, reply);
+    }
 };
 
-TickWorker tw;
+Ticker<10> ticker;
 
-IRQ_HANDLER(SysTick, tw.interrupt)
+IRQ_HANDLER(SysTick, ticker.interrupt)
 
-void tickWorker () {
-    auto wid = tw.init();
-    TEST_ASSERT_GREATER_THAN(0, wid);
+void msTicker () {
+    auto tickerId = ticker.init();
+    TEST_ASSERT_GREATER_THAN(0, tickerId);
 
     for (auto i = 1; i <= 250; ++i) {
         cycles::msBusy(1);
-        TEST_ASSERT_INT_WITHIN(1, i, tw.millis());
+        TEST_ASSERT_INT_WITHIN(1, i, ticker.millis());
+    }
+
+    Worker::send({ tickerId, ticker.RATE, 1 });
+
+    auto t = ticker.millis();
+    for (auto i = 1; i <= 50; ++i) {
+        cycles::msBusy(1);
+        TEST_ASSERT_INT_WITHIN(1, t+i, ticker.millis());
     }
 }
 
+struct TimerWorker : Worker {
+    enum TAG { START, ONE, TWO, THREE };
+
+    uint16_t start;
+    bool done =false;
+
+    Event process (Event in, Event out, void*) override {
+        switch (in.eTag) {
+            case START:
+                start = ticker.millis();
+                ticker.delay(5, { wId, ONE });
+                break;
+            case ONE:
+                TEST_ASSERT_INT_WITHIN(1, start+5, ticker.millis());
+                ticker.delay(10, { wId, TWO });
+                break;
+            case TWO:
+                TEST_ASSERT_INT_WITHIN(1, start+5+10, ticker.millis());
+                ticker.delay(20, { wId, THREE });
+done = true;
+                break;
+            case THREE:
+                TEST_ASSERT_INT_WITHIN(1, start+5+10+20, ticker.millis());
+                done = true;
+                break;
+            default:
+                fail();
+        }
+        return out;
+    }
+};
+
+void msWaiter () {
+    // TODO worker priority ordering
+    auto tickerId = ticker.init();
+    TEST_ASSERT_GREATER_THAN(0, tickerId);
+
+    Worker::send({ tickerId, ticker.RATE, 1 });
+
+    TimerWorker tw;
+    auto twId = tw.init();
+    TEST_ASSERT_GREATER_THAN(0, twId);
+
+    Worker::send({ twId, tw.START });
+
+    int n = 0;
+    do {
+        asm ("wfi");
+        ++n;
+    } while (!tw.done);
+    TEST_ASSERT_EQUAL(35, n);
+}
+
 void allTests () {
-    RUN_TEST(tickWorker);
+    RUN_TEST(msTicker);
+    RUN_TEST(msWaiter);
 }
