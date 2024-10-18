d9a641175f13fa3aad45ee109608de75e0e9df0e
Jean-Claude Wippler <jc@wippler.nl>
2024-10-18 20:25:40 +0200

    add i2c detect, it works on g431 but not lon l412

--- examples/g431k/defs.h
+++ examples/g431k/defs.h
@@ -4,43 +4,35 @@
 #define PIOENV  "i2c"
 
 //CG1 board leds
-#define LED  "B3"
+#define LED  "B8"
+
+const Pin led (LED,"P");
 
 //CG[ board uart
-#define UART_NAME  USART2
-#define UART_PINS  "A2:7,A15:3"
-#define UART_FREQ  80
-#define UART_CONF  Irq::DMA1_Channel7,Irq::DMA1_Channel6,1-1,7-1,6-1,2,2
+#define UART_NAME  USART1
+#define UART_PINS  "A9:U7,A10"
+#define UART_FREQ  170
+#define UART_CONF  Irq::DMA1_CH1,Irq::DMA1_CH2,1-1,1-1,2-1,25,24
 //CG]
 
 //CG[ board spi
 #define SPI_NAME  SPI1
 #define SPI_PINS  "B5:H5,B4,B3,A11:HP"
-#define SPI_FREQ  80
-#define SPI_TYPE  SPI1.ADDR,DMA1.ADDR,3-1,2-1
-#define SPI_CONF  {ena::SPI1,80,Irq::DMA1_Channel3,Irq::DMA1_Channel2}, {1-1,1,1}
+#define SPI_FREQ  170
+#define SPI_TYPE  SPI1.ADDR,DMA1.ADDR,3-1,4-1
+#define SPI_CONF  {ena::SPI1,170,Irq::DMA1_CH3,Irq::DMA1_CH4}, {1-1,11,10}
 //CG]
 
 //CG[ board i2c
 #define I2C_NAME  I2C1
-#define I2C_PINS  "B7:OH4,B6"
-#define I2C_FREQ  80
-#define I2C_TYPE  I2C1.ADDR,DMA2.ADDR,7-1,6-1
-#define I2C_CONF  {ena::I2C1,80,Irq::I2C1_EV,Irq::I2C1_ER}, {2-1,5,5}
+#define I2C_PINS  "B7:OH4,A15"
+#define I2C_FREQ  170
+#define I2C_TYPE  I2C1.ADDR,DMA1.ADDR,3-1,4-1
+#define I2C_CONF  {ena::I2C1,170,Irq::I2C1_EV,Irq::I2C1_ER}, {1-1,17,16}
 //CG]
 
 uint32_t i2cTiming (uint16_t khz, uint16_t mhz =SystemCoreClock/1'000'000) {
     switch (mhz) {
-      case 4: // MHz
-        switch (khz) {
-          //CG[ i2c timing 4
-          // 4 Mhz: (remove this line to re-generate)
-          case  100: return 0x00101110; // prs 0 tcd 1 tdd 0 scll 16 sclh 17
-          case  400: return 0x00100003; // prs 0 tcd 1 tdd 0 scll 3 sclh 0
-          // no valid solution for 1000 kHz @ 4 MHz
-          //CG]
-        }
-        break;
       case 16: // MHz
         switch (khz) {
           //CG[ i2c timing 16
@@ -48,16 +40,6 @@ uint32_t i2cTiming (uint16_t khz, uint16_t mhz =SystemCoreClock/1'000'000) {
           case  100: return 0x00504F49; // prs 0 tcd 5 tdd 0 scll 73 sclh 79
           case  400: return 0x00500D12; // prs 0 tcd 5 tdd 0 scll 18 sclh 13
           case 1000: return 0x00500205; // prs 0 tcd 5 tdd 0 scll 5 sclh 2
-          //CG]
-        }
-        break;
-      case 48: // MHz
-        switch (khz) {
-          //CG[ i2c timing 48
-          // 48 Mhz: (remove this line to re-generate)
-          case  100: return 0x0010F1DB; // prs 0 tcd 1 tdd 0 scll 219 sclh 241
-          case  400: return 0x00102E39; // prs 0 tcd 1 tdd 0 scll 57 sclh 46
-          case 1000: return 0x00100F13; // prs 0 tcd 1 tdd 0 scll 19 sclh 15
           //CG]
         }
         break;
@@ -71,6 +53,26 @@ uint32_t i2cTiming (uint16_t khz, uint16_t mhz =SystemCoreClock/1'000'000) {
           //CG]
         }
         break;
+      case 160: // MHz
+        switch (khz) {
+          //CG[ i2c timing 160
+          // 160 Mhz: (remove this line to re-generate)
+          case  100: return 0x30E0D4C1; // prs 3 tcd 14 tdd 0 scll 193 sclh 212
+          case  400: return 0x50901B22; // prs 5 tcd 9 tdd 0 scll 34 sclh 27
+          case 1000: return 0x40B00B0E; // prs 4 tcd 11 tdd 0 scll 14 sclh 11
+          //CG]
+        }
+        break;
+      case 170: // MHz
+        switch (khz) {
+          //CG[ i2c timing 170
+          // 170 Mhz: (remove this line to re-generate)
+          case  100: return 0x3010D4C1; // prs 3 tcd 1 tdd 0 scll 193 sclh 212
+          case  400: return 0x0050AACE; // prs 0 tcd 5 tdd 0 scll 206 sclh 170
+          case 1000: return 0x00503C49; // prs 0 tcd 5 tdd 0 scll 73 sclh 60
+          //CG]
+        }
+        break;
     }
     fail();
 }
@@ -81,8 +83,8 @@ namespace serio {
     void init () {
         Pin::config("A2:7");
         RCC(ena::USART2,1) = 1;
-        USART2[BRR] = SystemCoreClock / 2'000'000;
-        USART2[CR1] = (1<<3) | (1<<0); // TE UE
+        USART2[BRR] = SystemCoreClock / 10'000'000; // 160 MHz CPU clock
+        USART2[CR1] = (1<<29) | (1<<3) | (1<<0); // FIFOEN TE UE
     }
 
     void write (void const* ptr, int len) {
@@ -94,18 +96,20 @@ namespace serio {
     }
 }
 
-Pin led (LED,"P");
+extern "C" int _write (int fd, char* buf, int len) {
+    if (fd == 1)
+        serio::write(buf, len);
+    return len;
+}
+
+void jeeh::logWriter (void const* ptr, size_t len) {
+    serio::write(ptr, len);
+}
 
 void initBoard () {
+    fastClock(); // 160 MHz
     cycles::init();
-    rtc::init();
-#if 0
-    RCC[0x00](2) = 1; // MSIPLLEN
-    fastClock(false); // 48 MHz MSI
-    cycles::msBusy(3); // let MSI PLL stabilise
-#else
-    fastClock(); // 80 MHz
-#endif
+    rtc::init(false);
 
     serio::init();
 
@@ -117,14 +121,4 @@ void initBoard () {
             PIOENV, SVDNAME, SystemCoreClock / 1'000'000,
             dt.yr, dt.mo, dt.dy, dt.hh, dt.mm, dt.ss, (dt.ff * 1000) / 256);
     logf("\t i2c: %s spi: %s uart: %s", I2C_PINS, SPI_PINS, UART_PINS);
-}
-
-extern "C" int _write (int fd, char* buf, int len) {
-    if (fd == 1)
-        serio::write(buf, len);
-    return len;
-}
-
-void jeeh::logWriter (void const* ptr, size_t len) {
-    serio::write(ptr, len);
 }
---
+++ examples/g431k/i2c.cpp
@@ -0,0 +1,22 @@
+#include <jee.h>
+#include <jee/cycles.h>
+#include <jee/dma.h>
+#include <jee/i2c.h>
+using namespace jeeh;
+#include "defs.h"
+
+i2c::Gpio i2cGpio;
+
+int main () {
+    initBoard();
+
+    i2cGpio.init(I2C_PINS, 99);
+    i2c::detect(i2cGpio);
+
+    while (true) {
+        led = 1;
+        cycles::msBusy(100);
+        led = 0;
+        cycles::msBusy(900);
+    }
+}
