3a2db5adfa9cda808e1b1da812a2f7b47825ac9b
Jean-Claude Wippler <jc@wippler.nl>
2024-11-02 14:01:19 +0100

    implement "binary sliding-window convolution"

--- examples/f303o/conv.cpp
+++ examples/f303o/conv.cpp
@@ -2,11 +2,41 @@
 
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 
-const int16_t signal [] = {
+const int16_t capture [] = {
 #include "conv.h"
 };
 
+struct Convolution {
+    bool sig [1000] {};
+    int cnt =0, high =0, low =0, prev =0, pos =0;
+
+    int wrap (int n) const {
+        return sig[(n + 1000) % 1000];
+    }
+
+    void feed (bool f) {
+        auto idx = ++cnt % 1000;
+        sig[idx] = f;
+        // track count in (-900,-100]
+        low -= wrap(idx - 900);
+        low += wrap(idx - 100);
+        // track count in (-100,0]
+        high -= wrap(idx - 100);
+        high += wrap(idx - 0);
+        auto sum = (800 - low) + 8 * high;
+        if (sum > prev)
+            pos = cnt;
+        else if (pos == cnt-1 && prev > 1500)
+            printf("%d,%d\n", pos%1000, prev);
+        prev = sum;
+    }
+};
+
 int main () {
-    printf("%d %d\n", signal[0], sizeof signal * 16);
+    Convolution conv;
+    for (auto e : capture)
+        for (auto i = 0; i < 16; ++i)
+            conv.feed((e >> i) & 1);
 }
