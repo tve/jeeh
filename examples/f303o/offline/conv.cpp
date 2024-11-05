3abd066d948bac8b0a3fe899cd6dec3e22fdb697
Jean-Claude Wippler <jc@wippler.nl>
2024-11-05 09:23:48 +0100

    save stdout to file

--- examples/f303o/offline/conv.cpp
+++ examples/f303o/offline/conv.cpp
@@ -3,33 +3,33 @@
 #include <cstdio>
 #include <cstdlib>
 
-struct Convolution {
-    bool sig [1000] {};
-    int cnt =0, high =0, low =0, prev =0, pos =0;
-
-    int wrap (int n) const {
-        return sig[(n+1000) % 1000];
-    }
-
-    bool feed (bool f) {
-        auto idx = ++cnt % 1000;
-        sig[idx] = f;
-        // track low count in (-900,-100] and high count in (-100,0]
-        low += wrap(idx-100) - wrap(idx-900);
-        high += wrap(idx-0) - wrap(idx-100);
-        auto sum = (800 - low) + 8 * high;
-        if (sum > prev)
-            pos = cnt;
-        else if (pos == cnt-1 && prev > 1500) {
-            printf("%d,%d\n", pos%1000, prev);
-            return true;
-        }
-        prev = sum;
-        return false;
-    }
-};
-
 void test1 () {
+    struct Convolution {
+        bool sig [1000] {};
+        int cnt =0, high =0, low =0, prev =0, pos =0;
+
+        int wrap (int n) const {
+            return sig[(n+1000) % 1000];
+        }
+
+        bool feed (bool f) {
+            auto idx = ++cnt % 1000;
+            sig[idx] = f;
+            // track low count in (-900,-100] and high count in (-100,0]
+            low += wrap(idx-100) - wrap(idx-900);
+            high += wrap(idx-0) - wrap(idx-100);
+            auto sum = (800 - low) + 8 * high;
+            if (sum > prev)
+                pos = cnt;
+            else if (pos == cnt-1 && prev > 1500) {
+                printf("%d,%d\n", pos%1000, prev);
+                return true;
+            }
+            prev = sum;
+            return false;
+        }
+    };
+
     Convolution conv;
 
     // read stream from stdin, see stream.cpp for info about the RLE encoding
@@ -40,12 +40,17 @@ void test1 () {
                 peaks += conv.feed((bits >> i) & 1);
         count += rept;
     }
-    printf("peaks %d, count %d\n", peaks, count*16/1000);
+    fprintf(stderr, "peaks %d, count %d\n", peaks, count*16/1000);
 }
 
 int main () {
     auto e = getenv("T");
     auto t = e != nullptr ? atoi(e) : 0;
+    if (t > 0) {
+        char buf [20];
+        snprintf(buf, sizeof buf, "out-%d.txt", t);
+        freopen(buf, "w", stdout);
+    }
 
     switch (t) {
         case 1:  test1(); break;
