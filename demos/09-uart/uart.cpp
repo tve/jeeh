f982d1a6f101d627f2a936c7ccca1478636afb80
Jean-Claude Wippler <jc@wippler.nl>
2024-11-25 20:41:17 +0100

    add polled uart demo

---
+++ demos/09-uart/uart.cpp
@@ -0,0 +1,18 @@
+#include <jee.h>
+#include <jee/hal.h>
+using namespace jeeh;
+#include "defs.h"
+
+uart::Poll<USART2.ADDR> gps (ena::USART2, 50);
+
+int main () {
+    initBoard();
+    gps.init("A2:7,A3", 9600);
+
+    while (true) {
+        uint8_t ch;
+        gps.transfer(false, &ch, 1);
+        serio::write(&ch, 1);
+        led.toggle();
+    }
+}
