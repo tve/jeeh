7fe839c7f1dd85956d86ff07adea779ded5ef40b
Jean-Claude Wippler <jc@wippler.nl>
2024-12-11 10:53:21 +0100

    i2cTiming code no longer used

--- boards/test/defs.h
+++ boards/test/defs.h
@@ -1,7 +1,7 @@
 // Lines with "CG" control the code-generated parts of this file.
 
 //CG1 pio
-#define PIOENV  "sync"
+#define PIOENV  "poll"
 
 //CG1 board leds
 #define LED  "B8"
@@ -9,7 +9,7 @@
 const Pin led (LED,"P");
 
 //CG1 board mode
-#define MODE_SYNC 1
+#define MODE_POLL 1
 
 //CG[ board pins
 #define PINS_VCC "B6"
@@ -66,62 +66,6 @@ UART_TRIGGER(console)
     void I2C1_EV_IRQHandler () { (w).irqI2c(); } \
 }
 //CG]
-
-uint32_t i2cTiming (uint16_t khz, uint16_t mhz =SystemCoreClock/1'000'000) {
-    switch (mhz) {
-      case 16: // MHz
-        switch (khz) {
-          //CG[ i2c timing 16
-          // 16 Mhz: (remove this line to re-generate)
-          case  100: return 0x00504F49; // prs 0 tcd 5 tdd 0 scll 73 sclh 79
-          case  400: return 0x00500D12; // prs 0 tcd 5 tdd 0 scll 18 sclh 13
-          case 1000: return 0x00500205; // prs 0 tcd 5 tdd 0 scll 5 sclh 2
-          //CG]
-        }
-        break;
-      case 72: // MHz
-        switch (khz) {
-          //CG[ i2c timing 72
-          // 72 Mhz: (remove this line to re-generate)
-          case  100: return 0x40404742; // prs 4 tcd 4 tdd 0 scll 66 sclh 71
-          case  400: return 0x10C0232B; // prs 1 tcd 12 tdd 0 scll 43 sclh 35
-          case 1000: return 0x10C00B0F; // prs 1 tcd 12 tdd 0 scll 15 sclh 11
-          //CG]
-        }
-        break;
-      case 80: // MHz
-        switch (khz) {
-          //CG[ i2c timing 80
-          // 80 Mhz: (remove this line to re-generate)
-          case  100: return 0x10E0D4C0; // prs 1 tcd 14 tdd 0 scll 192 sclh 212
-          case  400: return 0xA0200609; // prs 10 tcd 2 tdd 0 scll 9 sclh 6
-          case 1000: return 0x10E00E11; // prs 1 tcd 14 tdd 0 scll 17 sclh 14
-          //CG]
-        }
-        break;
-      case 160: // MHz
-        switch (khz) {
-          //CG[ i2c timing 160
-          // 160 Mhz: (remove this line to re-generate)
-          case  100: return 0x30E0D4C1; // prs 3 tcd 14 tdd 0 scll 193 sclh 212
-          case  400: return 0x50901B22; // prs 5 tcd 9 tdd 0 scll 34 sclh 27
-          case 1000: return 0x40B00B0E; // prs 4 tcd 11 tdd 0 scll 14 sclh 11
-          //CG]
-        }
-        break;
-      case 170: // MHz
-        switch (khz) {
-          //CG[ i2c timing 170
-          // 170 Mhz: (remove this line to re-generate)
-          case  100: return 0x3010D4C1; // prs 3 tcd 1 tdd 0 scll 193 sclh 212
-          case  400: return 0x0050AACE; // prs 0 tcd 5 tdd 0 scll 206 sclh 170
-          case 1000: return 0x00503C49; // prs 0 tcd 5 tdd 0 scll 73 sclh 60
-          //CG]
-        }
-        break;
-    }
-    fail();
-}
 
 extern "C" int _write (int fd, char* buf, int len) {
     if (fd == 1 || fd == 2)
