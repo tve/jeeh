86c8463c246ae08c7b6f23d41bf138cd51e73556
Jean-Claude Wippler <jc@wippler.nl>
2024-12-16 15:27:06 +0100

    update board image

--- boards/g431k/all.cpp
+++ boards/g431k/all.cpp
@@ -49,7 +49,7 @@ void header (char const* text) {
 int main () {
     initBoard();
 
-#if 0
+#if 1
     i2cBus.init(1000);
     header("I2C - SCAN");   testScan();
     header("I2C - OLED");   testOled();
--- boards/g431k/defs.h
+++ boards/g431k/defs.h
@@ -1,14 +1,15 @@
 // Lines with "CG" control the code-generated parts of this file.
 
 //CG1 pio
-#define PIOENV  "block"
+#define PIOENV  "poll"
 
 //CG1 board leds
 #define LED  "B8"
 
 const Pin led (LED,"P");
 
-//CG: board mode
+//CG1 board mode
+#define MODE_POLL 1
 
 //CG[ board pins
 #define PINS_VCC "B6"
